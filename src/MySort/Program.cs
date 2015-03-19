6e9d876a16709da1f9d34ae8c1c77366665432b8
wicky <whe@xogrp.com>
2015-03-19 16:04:58 +0800

    init commit

---
+++ src/MySort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new[] {1, 6, 23, 8, 3, 0, 45, 9, 234,-1, 7, 56, 91, 123};
+            //arr.Bubble();            
+            //arr.Selection();
+            arr.Insertion();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/MySort/Sort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySort
+{
+    public static class Sort
+    {
+        public static void Bubble(this int[] arr)
+        {
+            int i, j;
+            int l = arr.Length;
+            for (i = 0; i < l; i++)
+            {
+                for (j = l - 1; j > i; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        int temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void Selection(this int[] arr)
+        {
+            int i, j;
+            int l = arr.Length;
+            for (i = 0; i < l; i++)
+            {
+                int min = i;
+                for (j = i + 1; j <= l - 1; j++)
+                {
+                    if (arr[min] > arr[j]) min = j;
+                }
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+
+        public static void Insertion(this int[] arr)
+        {
+            int i;
+            for (i = arr.Length - 1; i > 0; i--)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    int temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            for (i = 2; i < arr.Length; i++)
+            {
+                int j = i;
+                int temp = arr[i];
+                while (temp < arr[j - 1])
+                {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[j] = temp;
+            }
+        }
+
+    }
+}
