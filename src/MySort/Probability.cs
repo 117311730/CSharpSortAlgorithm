bf051ab1620ecf65653373e3d4446d8c56af3f59
wicky <whe@xogrp.com>
2015-04-09 17:51:12 +0800

    fix

--- src/MySort/Probability.cs
+++ src/MySort/Probability.cs
@@ -15,12 +15,12 @@ namespace MySort
             _rand = new Random();
             _prizeList = new Dictionary<int, Prize>();
 
-            _prizeList.Add(0, new Prize { Name = "平板电脑", Probability = 1 });
-            _prizeList.Add(1, new Prize { Name = "数码相机", Probability = 5 });
-            _prizeList.Add(2, new Prize { Name = "音箱设备", Probability = 10 });
-            _prizeList.Add(3, new Prize { Name = "4G优盘", Probability = 12 });
-            _prizeList.Add(4, new Prize { Name = "10Q币", Probability = 22 });
-            _prizeList.Add(5, new Prize { Name = "下次没准就能中哦", Probability = 50 });
+            _prizeList.Add(0, new Prize { Name = "下次没准就能中哦", Probability = 50 });
+            _prizeList.Add(1, new Prize { Name = "平板电脑", Probability = 1 });
+            _prizeList.Add(2, new Prize { Name = "数码相机", Probability = 5 });
+            _prizeList.Add(3, new Prize { Name = "音箱设备", Probability = 10 });
+            _prizeList.Add(4, new Prize { Name = "4G优盘", Probability = 12 });
+            _prizeList.Add(5, new Prize { Name = "10Q币", Probability = 22 });
         }
 
         public int? GetRand(int[] arr)
